2097ef56d67208ecdb23b7188326f55c5b5cf763
rms <webrmsky@gmail.com>
2021-10-13 22:37:32 +0800

    添加useMount和useDebounce两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { useEffect, useState } from "react"
 import { SearchPanel } from "./search-panel"
 import { List } from "./list"
-import { cleanObject } from "../../utils"
+import { cleanObject, useDebounce, useMount } from "../../utils"
 import * as qs from "qs"
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -14,20 +14,22 @@ export const ProjectListScreen = () => {
     })
     const [list, setList] = useState([])
     const [users, setUsers] = useState([])
+    const debouncedParam = useDebounce(param, 2000)
+    
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
             if (response.ok) {
                 setList(await response.json())
             }
         })
-    }, [param])
-    useEffect(() => {
+    }, [debouncedParam])
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async response => {
             if (response.ok) {
                 setUsers(await response.json())
             }
         })
-    }, [param])
+    })
 
     return <div>
         <SearchPanel users={users} param={param} setParam={setParam} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,8 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
+
+// 在一个函数里改变传入的对象本身是不好的
 export const cleanObject = (object) => {
   // Object.assign({}, object)
   const result = { ...object };
@@ -10,3 +14,23 @@ export const cleanObject = (object) => {
   });
   return result;
 };
+
+// 定义一个初始化一次执行hook
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+
+  return debouncedValue
+}
